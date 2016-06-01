adba06bfb8a42acaa0b438fda67bb8d2b8f9656f
codisart <louis.celier@gmail.com>
2016-06-01 19:08:38 +0200

    [TECH] Refactor the coercion service to get only valid files

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
 
 var FileSystem 	= require('fs');
 
-var program	= require('commander');
-var lib 	= require('./lib/identifier.js');
-var mover 	= require('./lib/mover.js');
-var validator	= require('./lib/file/validator.js');
+var program		= require('commander');
+var lib 		= require('./lib/identifier.js');
+var mover 		= require('./lib/mover.js');
+var coercion 	= require('./lib/coercion.js');
 
 program
 	.version('0.0.2')
 	.option('-K, --kind <n>', 'The kind of torrent', /^(single|multi)$/i, false)
-	.option('-D, --directory <n>', 'An valid directory path', validator.existDirectory, false)
-	.option('-F, --file <n>', 'An valid file path', validator.isMediaFile, false)
+	.option('-D, --directory <n>', 'An valid directory path', coercion.getValidFilesListFromDirectory, false)
+	.option('-F, --file <n>', 'An valid file path', coercion.getValidFilesListFromFile, false)
 	.parse(process.argv);
 
 
@@ -21,40 +21,36 @@ if(!program.kind) {
 }
 
 if (program.kind === 'single') {
-	if(!program.file) {
+	if(!program.file || program.directory.length <= 0) {
 		console.log('The filepath doesn\'t not exist');
 		process.exit(1);
 	}
-
 	var files = program.file;
 }
 
 if (program.kind === 'multi') {
-	if(!program.directory) {
+	if(!program.directory || program.directory.length <= 0) {
 		console.log('The directory doesn\'t not exist');
 		process.exit(1);
 	}
-
-	var files = FileSystem.readdirSync(program.directory);
+	var files = program.directory;
 }
 
-if (files.length > 0) {
-	var wasAtLeastOneFileProcessed = false;
-	for (var index in files) {
-		var filePath = files[index];
+var wasAtLeastOneFileProcessed = false;
+for (var index in files) {
+	var filePath = files[index];
 
-		var episode = lib.identifyEpisode(filePath);
+	var episode = lib.identifyEpisode(filePath);
 
-		if (!episode) {
-			console.log('We could not identify the tv show the season or the episode');
-			continue;
-		}
-
-		mover.store(episode);
-		wasAtLeastOneFileProcessed = true;
+	if (!episode) {
+		console.log('We could not identify the tv show the season or the episode');
+		continue;
 	}
 
-	if (wasAtLeastOneFileProcessed) {
-		console.log('delete all other files');
-	}
+	mover.store(episode);
+	wasAtLeastOneFileProcessed = true;
+}
+
+if (wasAtLeastOneFileProcessed) {
+	console.log('delete all other files');
 }
