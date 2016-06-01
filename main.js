ff2a01f5651445d14f409748b41ca17f317b743e
codisart <louis.celier@gmail.com>
2016-06-01 17:26:43 +0200

    [TECH] put the files existence validation in a extern library

--- main.js
+++ main.js
@@ -5,40 +5,13 @@ var FileSystem 	= require('fs');
 var program	= require('commander');
 var lib 	= require('./lib/identifier.js');
 var mover 	= require('./lib/mover.js');
-
-function existDirectory(path) {
-	try {
-		if (FileSystem.statSync(path).isDirectory()) {
-			return path;
-		}
-		else {
-			return false;
-		}
-	}
-	catch (e) {
-		return false;
-	}
-}
-
-function existFile(path) {
-	try {
-		if (FileSystem.statSync(path).isFile()) {
-			return path;
-		}
-		else {
-			return false;
-		}
-	}
-	catch (e) {
-		return false;
-	}
-}
+var fileValidator	= require('./lib/file/validator.js');
 
 program
 	.version('0.0.2')
 	.option('-K, --kind <n>', 'The kind of torrent', /^(single|multi)$/i, false)
-	.option('-D, --directory <n>', 'An valid directory path', existDirectory, false)
-	.option('-F, --file <n>', 'An valid file path', existFile, false)
+	.option('-D, --directory <n>', 'An valid directory path', fileValidator.existDirectory, false)
+	.option('-F, --file <n>', 'An valid file path', fileValidator.existFile, false)
 	.parse(process.argv);
 
 
